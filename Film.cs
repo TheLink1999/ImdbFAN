865e456c9c599fa44c9eac8aac29ac936430dd7f
user1 <gfg>
2017-05-26 13:34:17 +0400

    new version

---
+++ Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imdb
+{
+    class Film : IComparable
+    {
+        
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
