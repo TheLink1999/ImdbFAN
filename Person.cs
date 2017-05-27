c765cc6dcfb3bea3878fb370d48de337b01133ba
user1 <gfg>
2017-05-27 13:36:45 +0400

    person form interfase > class, and create critic class

--- Person.cs
+++ Person.cs
@@ -6,9 +6,49 @@ using System.Threading.Tasks;
 
 namespace Imdb
 {
-    interface IPerson
+    class Person
     {
-        string Name { get; set; }
-        string Phone { get; set; }
+        //fields
+        private string name;
+        private string phone;
+        //proprties
+        string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+            }
+        }
+        string Phone
+        {
+            get
+            {
+                return phone;
+            }
+            set
+            {
+                if (value[0] == '0' && value.Length == 9)
+                {
+                    phone = value;
+                }
+                else
+                {
+                    throw new Exception(" Please enter your phoneNomber just like this //094526314 ");
+                }
+            }
+        }
+        //constractors
+        public Person(string n, string p)
+        {
+            Name = n;
+            Phone = p;
+        }
+        //Methods
+        public override string ToString()
+        {
+            return name + " : " + phone;
+        }
     }
+
 }
